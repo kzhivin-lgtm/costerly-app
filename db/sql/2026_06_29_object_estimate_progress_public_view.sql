b1a628c69742b84f21ba395a9fd19a63a69d4e25
Kirill Zhivin <qb@Kirills-MacBook-Air.local>
2026-06-29 23:05:27 +0300

    v2.01.20 fix progress view column order

--- db/sql/2026_06_29_object_estimate_progress_public_view.sql
+++ db/sql/2026_06_29_object_estimate_progress_public_view.sql
@@ -7,12 +7,12 @@ with (security_invoker = false) as
 select
     estimate_id,
     object_id,
-    quantity,
     status,
-    self_cost_ex_vat,
     progress_percent,
     progress_label,
-    progress_updated_at
+    progress_updated_at,
+    quantity,
+    self_cost_ex_vat
 from public.rfq_object_estimates;
 
 grant select on public.rfq_object_estimate_progress_public to anon;
